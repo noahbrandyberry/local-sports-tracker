ea99ce5449a63d7a40428481a589fe84bcc28d2e
Noah Brandyberry <nbrandyberry@edgewebware.com>
2022-01-24 05:31:37 -0500

    fix: bugs before app store push

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
   baseUrl: __DEV__
     ? 'http://localhost:3000/api/v1'
-    : 'https://myschoolsports.herokuapp.com/api/v1',
+    : 'https://www.myschoolsports.us/api/v1',
 };
 
 export default config;
