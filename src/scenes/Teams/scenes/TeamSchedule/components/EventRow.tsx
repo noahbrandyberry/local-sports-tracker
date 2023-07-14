b370a1dbdfc2a52ef4ace2bf821a203a315a58c1
Noah Brandyberry <nbrandyberry@edgewebware.com>
2023-07-14 14:01:54 -0400

    chore: bug with row

--- src/scenes/Teams/scenes/TeamSchedule/components/EventRow.tsx
+++ src/scenes/Teams/scenes/TeamSchedule/components/EventRow.tsx
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    marginTop: 12,
+    marginBottom: 12,
     padding: 15,
   },
   nameContainer: {
